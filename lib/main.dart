2931cfc6f47e46164d54a94af9d06de419decd7a
Ethjin8 <ethjin8@gmail.com>
2024-02-18 15:40:07 -0800

    Small fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-import 'package:classtime_app/pages/auth_page.dart';
+import 'package:classtime_app/pages/authentication/auth_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: AuthPage(),
     );
